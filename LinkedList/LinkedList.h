3ba94d4c71f43b82964447e8930b4b95501ba81e
Jonathan Junio <jonathan.jm.nunes@gmail.com>
2021-03-05 16:58:03 -0300

    created main for testing linked lists

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -3,3 +3,5 @@
 p_node create_list();
 
 void destroy_list(p_node list);
+
+p_node add_element(int x, p_node list);
--- LinkedList/linkedList.c
+++ LinkedList/linkedList.c
@@ -12,3 +12,18 @@ void destroy_list(p_node list) {
     }
     free(list);
 }
+
+p_node add_element(int x, p_node list) {
+    p_node new_node;
+    new_node = malloc(sizeof(Node));
+    new_node->data = x;
+    new_node->next = list;
+    return new_node;
+}
+
+void print_list(p_node list) {
+    if (!list->next == NULL) {
+        print_list(list->next);
+    }
+    printf("%d ", list->data);
+}
