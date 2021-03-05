af54b790fa88e0ff89338fb65a6dfe508f6be2ff
Jonathan Junio <jonathan.jm.nunes@gmail.com>
2021-03-05 16:58:17 -0300

    created main for testing linked lists

---
+++ tests/linked_lists.c
@@ -0,0 +1,17 @@
+#include <../LinkedList/LinkedList.h>
+
+int main(int argc, char const *argv[])
+{
+    int n, i;
+    p_node list;
+    list = create_list();
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        list = add_element(i+1, list);
+    }
+    print_list(list);
+    destroy_list(list);
+    
+    return 0;
+}
